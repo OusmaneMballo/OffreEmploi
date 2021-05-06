9a2cdbc5929bd30b52aa107f1dd0ce706700590b
Ousmane MBALLO <ousmane.mballo@fahpo.com>
2021-05-06 02:41:43 +0000

    Ajout d'une nouvelle demande

--- src/Controller/DemandeController.php
+++ src/Controller/DemandeController.php
@@ -2,12 +2,30 @@
 
 namespace App\Controller;
 
+use App\Entity\Demande;
+use App\Entity\Domaine;
+use App\Entity\Offre;
+use App\service\FileDeleteService;
+use Doctrine\ORM\EntityManagerInterface;
+use phpDocumentor\Reflection\Types\This;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\service\FileUploader;
 
 class DemandeController extends AbstractController
 {
+    private $em;
+    private $offreRepository;
+    private $demandeRepository;
+
+    public function __construct(EntityManagerInterface $emi)
+    {
+        $this->em=$emi;
+        $this->demandeRepository=$this->em->getRepository(Demande::class);
+        $this->offreRepository=$this->em->getRepository(Offre::class);
+    }
     /**
      * @Route("/demande", name="app_demande")
      */
@@ -17,4 +35,75 @@ class DemandeController extends AbstractController
             'controller_name' => 'DemandeController',
         ]);
     }
+
+    /**
+     * @Route("/addDemande", name="app_demande_add", methods={"POST"})
+     * @param Request $request
+     * @return Response
+     */
+    public function addDemande(Request $request, string $uploadLettre, FileUploader $uploaderFile, string $uploadDirCv, FileDeleteService $fileDeleteService): Response
+    {
+        if ($request->isMethod("POST")){
+            if ($this->isCsrfTokenValid('candidature', $request->request->get('candidature_token'))){
+                $demande=new Demande();
+                //Cas ou le candidat saisi sa lettre de motivatio
+                if($request->request->get('motivationtxt')!=''){
+                    $demande->setTextMotivation($request->request->get('motivationtxt'));
+                }
+                else{
+                    //Cas ou le candidat charge une lettre de motivation a partir de son device
+                    if(!empty($request->files->get('motivationfile'))){
+                        $lettreFile = $request->files->get('motivationfile');
+
+                        //Recuperation du nom du fichier
+                        $lettrename = $lettreFile->getClientOriginalName ();
+
+                        //On appelle le service de chargement du fichier dans le repertoir specifié
+                        $ok=$uploaderFile->upload($uploadLettre,$lettreFile,$lettrename);
+
+                        //On teste si le fichier est bien chargé
+                        if(!$ok){
+                            //return $this->render('demandeur/index.html.twig', ["demandeur"=>$demandeur]);
+                        }
+                        else{
+                            $demande->setTextMotivation($lettrename);
+                        }
+                    }
+                    else{
+                        //Message d'erreur!
+                    }
+                }
+                //Cas ou le candidat charge un nouveau cv a partir de son device
+                if (!empty($request->files->get('cv'))){
+                    //On charge maintenant son cv
+                    $cvFile = $request->files->get('cv');
+                    //Recuperation du nom du fichier
+                    $cvname = $cvFile->getClientOriginalName ();
+
+                    //On appelle le service de chargement du fichier dans le repertoir specifié
+                    $ok=$uploaderFile->upload($uploadDirCv,$cvFile,$cvname);
+
+                    //On teste si le fichier est bien chargé
+                    if(!$ok){
+                        //Erreur
+                    }
+                    else{
+                        $demande->setCv($cvname);
+                    }
+                }
+                else{
+                    $demande->setCv($this->getUser()->getDemandeur()->getCv());
+                }
+                $demande->setDate(date("Y-m-d H:i:s"));
+                $demande->setDemandeur($this->getUser()->getDemandeur());
+                $demande->setOffre($this->offreRepository->find($request->request->get('id_offre')));
+                $demande->setReponse("En cours");
+                $this->em->persist($demande);
+                $this->em->flush();
+            }
+        }
+        return $this->render('demande/index.html.twig');
+    }
+
+
 }
